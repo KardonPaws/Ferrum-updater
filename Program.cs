9edecf1c074b174e36cf2f28d93c1ed248739935
Igor Varlamov <v.i.p.0009@mail.ru>
2024-11-23 23:35:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+
+class UpdaterProgram
+{
+
+    [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr h, string m, string c, int type);
+    static void Main(string[] args)
+    {
+        Process[] procs = Process.GetProcessesByName("revit");
+        foreach (Process process in procs)
+        {
+            process.WaitForExit();
+        }
+        MessageBox((IntPtr)0, "Обновление начато", "Железно", 0);
+        var fileName = "FerrumAddin.dll";
+        string downloadDir = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+        var localPath = Path.Combine(downloadDir, fileName);
+
+        var tempPath = Path.Combine(downloadDir, "new" + fileName);
+        if (File.Exists(tempPath))
+        {
+            File.Delete(localPath);
+            File.Move(tempPath, localPath);
+            Console.WriteLine($"{fileName} был обновлен.");
+            MessageBox((IntPtr)0, "Обновление завершено", "Железно", 0);
+        }
+    }
+
+}
